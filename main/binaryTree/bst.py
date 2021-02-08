9574e5196dd095b96fffe6e21598ecbadf4e08f9
Ameen <ameen.nazar@emaratech.ae>
2021-02-08 16:56:23 +0400

    DS - standard tree

--- main/binaryTree/bst.py
+++ main/binaryTree/bst.py
@@ -1,33 +1,36 @@
-class BinarySearchTree:
+class BinarySearchTreeNode:
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
     def add_child(self, data):
-        if self.data == data:
-            return
-        if self.data > data:
+        if data == self.data:
+            return # node already exist
+
+        if data < self.data:
             if self.left:
                 self.left.add_child(data)
             else:
-                self.left = BinarySearchTree(data)
+                self.left = BinarySearchTreeNode(data)
         else:
             if self.right:
                 self.right.add_child(data)
             else:
-                self.right = BinarySearchTree(data)
+                self.right = BinarySearchTreeNode(data)
 
-    def search(self,val):
+
+    def search(self, val):
         if self.data == val:
             return True
 
-        if self.data > val:
+        if val < self.data:
             if self.left:
                 return self.left.search(val)
             else:
                 return False
-        else:
+
+        if val > self.data:
             if self.right:
                 return self.right.search(val)
             else:
@@ -35,43 +38,14 @@ class BinarySearchTree:
 
     def in_order_traversal(self):
         elements = []
-        # left
         if self.left:
             elements += self.left.in_order_traversal()
-        #node
+
         elements.append(self.data)
-        #right
+
         if self.right:
             elements += self.right.in_order_traversal()
-        return elements
 
-    def find_min(self):
-        if self.left:
-            return self.left.find_min()
-        else:
-            return self.data
-
-    def find_max(self):
-        if self.right:
-            return self.right.find_max()
-        else:
-            return self.data
-
-    def calculate_sum(self):
-        sum = 0
-        if self.left:
-            sum += self.left.calculate_sum()
-        sum += self.data
-        if self.right:
-            sum += self.right.calculate_sum()
-        return sum
-
-    def pre_order_traversal(self):
-        elements = [self.data]
-        if self.left:
-            elements += self.left.pre_order_traversal()
-        if self.right:
-            elements += self.right.pre_order_traversal()
         return elements
 
     def post_order_traversal(self):
@@ -80,18 +54,48 @@ class BinarySearchTree:
             elements += self.left.post_order_traversal()
         if self.right:
             elements += self.right.post_order_traversal()
+
         elements.append(self.data)
+
         return elements
 
+    def pre_order_traversal(self):
+        elements = [self.data]
+        if self.left:
+            elements += self.left.pre_order_traversal()
+        if self.right:
+            elements += self.right.pre_order_traversal()
+
+        return elements
+
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+
+    def calculate_sum(self):
+        left_sum = self.left.calculate_sum() if self.left else 0
+        right_sum = self.right.calculate_sum() if self.right else 0
+        return self.data + left_sum + right_sum
 
 def build_tree(elements):
-    root = BinarySearchTree(elements[0])
-    for i in range(1, len(elements)):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
         root.add_child(elements[i])
+
     return root
 
 if __name__ == '__main__':
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+
     numbers = [15,12,7,14,27,20,23,88 ]
+
     numbers_tree = build_tree(numbers)
     print("Input numbers:",numbers)
     print("Min:",numbers_tree.find_min())
